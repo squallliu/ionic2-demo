d5f865f99f9b3a0334e89e681a1b770d5e0cc940
squall <squall_lau@qq.com>
2017-02-23 12:02:50 +0800

    加打包自动压缩图片脚本

---
+++ hooks/before_prepare/minify-image.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+function minifyimg() {
+  var gulp = require('gulp');
+  var imagemin = require('gulp-imagemin');
+  var pngquant = require('imagemin-pngquant');
+  gulp.task('minifyimg', function () {
+    return gulp.src('www/assets/images/**/*').pipe(imagemin({
+      progressive: true,
+      svgoPlugins: [{ removeViewBox: false }],
+      use: [pngquant()]
+    })).pipe(gulp.dest('www/assets/images'));
+  });
+
+  gulp.start('minifyimg');
+}
+
+minifyimg();
