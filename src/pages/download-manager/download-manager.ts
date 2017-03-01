2fb6d6b9adc63ad94cefd55dfd19751347133be2
am-wangjin <am-wangjin@gmail.com>
2017-03-01 09:15:20 +0800

    改名

--- src/pages/download-manager/download-manager.ts
+++ src/pages/download-manager/download-manager.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { DownloadManagerController, isPresent, DownloadManagerCmp } from 'ext-ionic';
+import { isPresent } from 'ext-ionic';
 
 @Component({
   selector: 'page-download-manager',
@@ -10,18 +10,13 @@ export class DownloadManagerPage {
   url: string;
   fileName: string;
 
-  constructor(private navCtrl: NavController, private download: DownloadManagerController) {
+  constructor(private navCtrl: NavController) {
     this.url = 'http://10.68.19.121:8101/1.PAK';
     this.fileName = '1.PAK';
   }
 
   onDownload() {
     if (isPresent(this.url) && isPresent(this.fileName)) {
-      this.download.download({ url: this.url, filePath: '/temp', fileName: this.fileName })
-        .catch(e => {
-          console.log(e);
-        });
-      this.navCtrl.push(DownloadManagerCmp);
     }
   }
 }
