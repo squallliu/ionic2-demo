526cbbf52a3335f94214227ea304b685d82b211c
am-wangjin <am-wangjin@gmail.com>
2017-03-08 16:46:53 +0800

    下载url无效提示

--- src/pages/download-manager/download-manager.ts
+++ src/pages/download-manager/download-manager.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { DownloadManagerController, DownloadOptions, DownloadManagerCmp } from 'ext-ionic';
+import { DownloadManagerController, DownloadOptions, DownloadManagerCmp, Dialog } from 'ext-ionic';
 
 @Component({
   selector: 'page-download-manager',
@@ -9,13 +9,16 @@ import { DownloadManagerController, DownloadOptions, DownloadManagerCmp } from '
 export class DownloadManagerPage {
   downloadOptions: DownloadOptions;
 
-  constructor(private navCtrl: NavController, private downloadManagerCtrl: DownloadManagerController) {
-    this.downloadOptions = { url: 'http://10.68.19.121:8101/1.PAK', fileName: '1.PAK' };
+  constructor(private navCtrl: NavController,
+    private downloadManagerCtrl: DownloadManagerController,
+    private dialog: Dialog) {
+    this.downloadOptions = { url: 'http://10.68.19.121:8101/1.PAK', fileName: '1.PAK', filePath: 'temp/' };
   }
 
   onDownload() {
     this.downloadManagerCtrl.download(this.downloadOptions)
       .catch(e => {
+        this.dialog.alert('', '无效的下载地址');
         console.log(e);
       });
     this.onDownloadManager();
