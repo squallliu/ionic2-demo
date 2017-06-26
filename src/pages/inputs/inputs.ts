a9f917f1adaf48208cf13273f368bad65a5719ef
squall <squall_lau@qq.com>
2017-06-26 15:05:37 +0800

    处理初始化值

--- src/pages/inputs/inputs.ts
+++ src/pages/inputs/inputs.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavParams, IonicPage } from 'ionic-angular';
 import { BaiduPlacesProvider } from 'ext-ionic';
 
@@ -11,7 +11,10 @@ export class InputsPage {
   fromTabs: boolean;
   starLevel: number = 4.5;
   dateTime: number = Date.now();
-  coord: any = {};
+  coord: any = {
+    lat: 30.54976553037,
+    lng: 114.28819134072
+  };
 
   constructor(private navParams: NavParams, private bpProvider: BaiduPlacesProvider) {
   }
