5cf69aac57dab279c7c6ef6731fe46c0449d25b1
squall <squall_lau@qq.com>
2017-10-23 08:58:41 +0800

    整理代码

--- src/pages/baidu-map/baidu-map.ts
+++ src/pages/baidu-map/baidu-map.ts
@@ -22,7 +22,7 @@ export class BaiduMapPage {
       city: 'ShangHai',
       markers: [{
         point: point,
-        icon: './assets/images/baidu-map/mappoint.png',
+        icon: './assets/imgs/baidu-map/mappoint.png',
         size: {
           width: 49,
           height: 60
