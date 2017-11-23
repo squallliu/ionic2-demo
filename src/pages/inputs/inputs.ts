6a40e0da6b826844a48b4349224f9571d731b637
squall <squall_lau@qq.com>
2017-11-23 15:26:39 +0800

    整理代码

--- src/pages/inputs/inputs.ts
+++ src/pages/inputs/inputs.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavParams } from 'ionic-angular';
-import { BaiduPlacesProvider } from 'ext-ionic';
+import { BaiduPlacesProvider, Dialog } from 'ext-ionic';
 
 @IonicPage()
 @Component({
@@ -16,9 +16,8 @@ export class InputsPage {
     lng: 114.28819134072
   };
 
-  constructor(private navParams: NavParams, public placesProvider: BaiduPlacesProvider) {
-    console.log(this.navParams.get('username'));
-    console.log(this.navParams.get('password'));
+  constructor(private navParams: NavParams, public placesProvider: BaiduPlacesProvider, private dialog: Dialog) {
+    this.dialog.alert('提示', '账号：' + this.navParams.get('account') + '\n密码：' + this.navParams.get('password'));
   }
 
   starChange($event) {
