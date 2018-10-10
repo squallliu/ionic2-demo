7b3224e3964c9dfe2189b71ced4b56f8aabcae8f
squall <squall_lau@qq.com>
2018-10-10 14:57:41 +0800

    整理代码

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,6 @@ import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { Deeplinks } from '@ionic-native/deeplinks';
-import { HotUpdater } from 'ext-ionic';
 
 @Component({
   templateUrl: 'app.html'
@@ -15,25 +13,9 @@ export class MyApp {
   constructor(
     private platform: Platform,
     private statusBar: StatusBar,
-    private splashScreen: SplashScreen,
-    private hotUpdater: HotUpdater,
-    private deeplinks: Deeplinks
+    private splashScreen: SplashScreen
   ) {
     this.platform.ready().then(() => {
-      this.deeplinks.routeWithNavController(this.nav, {
-        '/inputs': 'InputsPage'
-      }).subscribe((match) => {
-        // match.$route - the route we matched, which is the matched entry from the arguments to route()
-        // match.$args - the args passed in the link
-        // match.$link - the full link data
-        console.log('Successfully matched route', match);
-      }, (nomatch) => {
-        // nomatch.$link - the full link data
-        console.error('Got a deeplink that didn\'t match', nomatch);
-      });
-
-      this.hotUpdater.start();
-
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
